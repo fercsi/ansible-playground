4e164134efee958feda4b6abe27c93627331bb55
Ferenc Vajda <github@fercsi.com>
2024-03-09 15:37:30 +0000

    Upgrade resources

--- controller/docker-entrypoint.sh
+++ controller/docker-entrypoint.sh
@@ -8,17 +8,18 @@ APG_HOST_PASSWORD=${APG_HOST_PASSWORD:-root}
 IFS=' '
 read -r -a hosts <<< "$APG_HOSTS"
 
-if [ ! -f /root/.ssh/id_rsa ]; then
-    ssh-keygen -t rsa -b 2048 -N '' -f /root/.ssh/id_rsa
+if [ ! -f /root/.ssh/id_ed25519 ]; then
+    ssh-keygen -t ed25519 -N '' -f /root/.ssh/id_rsa
 fi
 
 if [ ! -f /var/run/ansible-playground ]; then
     echo "Add hosts to inventory: $APG_HOSTS"
+    mkdir -p /etc/ansible
     for h in "${hosts[@]}"; do
         echo "$h:"
         ssh-keyscan -H $h >>/root/.ssh/known_hosts
         echo $APG_HOST_PASSWORD | sshpass ssh-copy-id -f "root@$h"
-        #echo $h >>/etc/ansible/hosts
+        echo $h >>/etc/ansible/hosts
     done
     echo [$(date -Iseconds)] Inventory initialized. > /var/run/ansible-playground
 fi
@@ -28,4 +29,3 @@ if [ ! -z "$APG_HOSTS_FILE" ]; then
 fi
 
 exec "$@"
-
